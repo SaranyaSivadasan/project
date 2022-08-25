1aff8e13e12ffd579e97a219e59c6c608a39bf46
SaranyaSivadasan <68867201+SaranyaSivadasan@users.noreply.github.com>
2022-08-25 19:17:11 +0530

    Update Problem-4.java
    I have considered this as a array of elements and then finding the number of multiples.

--- Problem-4.java
+++ Problem-4.java
@@ -1,1 +1,24 @@
+import java.io.*;
+import java.util.*;
 
+class Program4 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Enter the total number of elements in the list : ");
+        int n = sc.nextInt();
+        int arr[] = new int[n];
+        int multiples[] = new int[9];
+        System.out.println("Enter the list of numbers : ");
+        for(int i=0;i<n;i++){
+            arr[i] = sc.nextInt();
+            for(int j=1;j<10;j++){
+                if(arr[i] % j == 0 ){
+                    multiples[j-1]++;
+                }
+            }
+        }
+        for(int k=0;k<9;k++){
+            System.out.println(k+1 + ":"+multiples[k]);
+        }
+    }
+}
