d4ed38e6f1b4b14bac3a3665e3575b6fd208ed44
SaranyaSivadasan <68867201+SaranyaSivadasan@users.noreply.github.com>
2022-08-25 18:51:30 +0530

    Update Problem-3.java

--- Problem-3.java
+++ Problem-3.java
@@ -1,1 +1,13 @@
+import java.io.*;
+import java.util.*;
 
+class Program3 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int x = sc.nextInt();
+        int num = (x % 2 == 0) ? x-1 :  x;
+        for(int i=1;i<num*2;i=i+2){
+            System.out.print(i+" ");
+        }
+    }
+}
