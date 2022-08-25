2a15470805ce85fae4b529a930320c9c47ffd989
SaranyaSivadasan <68867201+SaranyaSivadasan@users.noreply.github.com>
2022-08-25 13:55:11 +0530

    Update Problem-2.java

--- Problem-2.java
+++ Problem-2.java
@@ -1,1 +1,12 @@
+import java.io.*;
+import java.util.*;
 
+class Program2 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int x = sc.nextInt();
+        for(int i=1;i<x*2;i=i+2){
+            System.out.print(i+" ");
+        }
+    }
+}
