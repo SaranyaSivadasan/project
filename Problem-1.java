db230a147a22996df225cbc6916528e28c033359
SaranyaSivadasan <68867201+SaranyaSivadasan@users.noreply.github.com>
2022-08-25 13:41:53 +0530

    Update Problem-1.java

--- Problem-1.java
+++ Problem-1.java
@@ -1,1 +1,24 @@
+import java.io.*;
+import java.util.*;
 
+class Program1 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        //System.out.println("Enter input in following order: first number, second number, operator.");
+        double firstInput = sc.nextInt();
+        double secondInput = sc.nextInt();
+        String operator = sc.next();
+        switch (operator) {
+            case "+" : System.out.println(firstInput + secondInput);
+                        break;
+            case "-" : System.out.println(firstInput - secondInput);
+                        break;
+            case "*" : System.out.println(firstInput * secondInput);
+                        break;
+            case "/" : System.out.println(firstInput / secondInput);
+                        break;
+            default : System.out.println("Please enter a valid operator");
+        }
+        
+    }
+}
